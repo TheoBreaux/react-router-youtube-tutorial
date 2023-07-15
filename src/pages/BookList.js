37e948c8157c4f1c1cd894c27be1a09679486f1a
Theo Breaux <theobreaux@gmail.com>
2023-07-14 21:17:22 -0700

    first commit

---
+++ src/pages/BookList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const BookList = () => {
+  return (
+    <>
+      <h1>BookList</h1>
+      <Link to="/books/1">Book 1</Link>
+      <Link to="/books/2">Book 2</Link>
+    </>
+  );
+};
+
+export default BookList;
